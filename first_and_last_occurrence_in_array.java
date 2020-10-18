f3ae08e4becfff2403602a9aa4df47c7d0d2d83a
AnuragMishra25 <anuragmishra.manit@gmail.com>
2020-10-18 12:59:19 +0800

    Update first_and_last_occurrence_in_array.java

--- first_and_last_occurrence_in_array.java
+++ first_and_last_occurrence_in_array.java
@@ -1,35 +1,44 @@
 /*package whatever //do not write package name here */
-
+/*https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/*/
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class GFG {
-	public static void main (String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int testCases = sc.nextInt();
-		for(int i = 0;i< testCases;i++){
-		    int inputLength = sc.nextInt();
-		    int numToFind = sc.nextInt();
-		    int[] arr = new int[inputLength];
-		    int ind = 0;
-		    while(ind < inputLength){
-		        arr[ind] = sc.nextInt();
-		        ind++;
-		    }
-		  //  printArr(arr, inputLength);
-		}
-	}
-	
-	public static void printArr(int[] arr, int size){
-	    for(int i=0;i< size;i++){
-	        System.out.println(arr[i]);
-	    }
-	}
-	
-	public static int findFirstOccurrence(int arr, int size){
-	    int start= 0, end = size-1;
-	    
-	    return [];
-	}
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = firstOccurrence(nums, target);
+        result[1] = lastOccurrence(nums, target);
+        return result;
+    }
+    
+    public static int firstOccurrence(int[] nums, int target){
+        int start = 0, end = nums.length-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target && (mid == 0 || nums[mid-1] < target )){
+                return mid;
+            }else if(nums[mid] < target){
+                start = mid +1;
+            }else{
+                end = mid -1 ;
+            }
+        }
+        return -1;
+    }
+    
+    public static int lastOccurrence(int[] nums, int target){
+        int start = 0, end = nums.length-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target && (mid+1 == nums.length || nums[mid+1] > target)){
+                return mid;
+            }else if(target < nums[mid]){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
 }
